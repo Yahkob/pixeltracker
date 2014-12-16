be919f7012142df203440eee5a8bca01f3a7892f
Jake Harclerode <Jakobharclerode@gmail.com>
2014-12-16 10:30:50 -0800

    clean up code

--- server.js
+++ server.js
@@ -20,16 +20,23 @@ app.listen(port, function(){
  
 app.get('/*', function (req, res, next) {
   var returnStr = '';
+  
   returnStr += 'user-agent: ' + req.headers['user-agent'] + '<br> hostname: ' + req.hostname + 
   '<br> remoteAdress: ' + req.connection.remoteAddress + '<br> domain:' + req.connection.domain;
+  
   res.sendFile('./assets/tp.gif', {"root": __dirname});
+  
   transporter.sendMail({
     from: 'pixelTracker ✔ <pixelTracker@gmail.com>', 
     to: 'jakobharclerode@gmail.com', 
     subject: 'Mail opened ✔', 
     html: returnStr,
   }, function (err, info) {
-      if (err) {return err;}
-      else {return info;}
+      if (err) {
+        return err;
+      } else {
+        return info;
+      }
   });
+  
 });
